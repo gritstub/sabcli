65ab86bc30607e1f6167898e55ea21972c9c6e80
HenrikDK <noreply@email.com>
2015-06-21 21:57:32 +0200

    restructured to decrease interface between presenter and curses api

--- sabcli/presenters/sabMiscPresenter.py
+++ sabcli/presenters/sabMiscPresenter.py
@@ -1,46 +1,58 @@
-import  sys
+from cursesUI.cursesWindow import cursesWindow
+
 
 class sabMiscPresenter():
-    def __init__(self):
-        self.window = sys.modules["__main__"].window
+    def __init__(self, window = None):
+        if not window:
+            window = cursesWindow()
+        self.window = window
+
+        self.screen = []
+        self.pad = []
 
     def display(self, apiInfo = {}):
-        self.window.addString(2, 3, 'Option - Description\n')
+        self.screen = []
+        self.pad = []
+
+        self.displayGeneralInformation(apiInfo)
+        self.displayMiscellaneousInformation(apiInfo)
+
+        self.window.draw(self.screen)
+        self.window.pad.draw(self.pad)
+
+    def displayGeneralInformation(self, apiInfo):
+        self.screen.append((3, 2, 'Option - Description\n', ''))
         version = 'SABCurses: ' + apiInfo['version']
-        self.window.addString(2, int(self.window.size[1]) - len(version), version)
-        self.window.addString(3, 0, '-' * int(self.window.size[1] + '\n'))
-        self.window.addStr('\n')
+        self.screen.append((int(self.window.size[1]) - len(version), 2, version, ''))
+        self.screen.append((0, 3, '-' * int(self.window.size[1] + '\n'), ''))
 
-        self.window.pad.addStr('   Add file or link')
-        self.window.pad.addString(0, 35, '- Add a nzb file location or URL to sabnzbd+')
-        self.window.pad.addString(1, 8, 'URL or file :\n\n')
+    def displayMiscellaneousInformation(self, apiInfo):
+        self.pad.append((3,  0, 'Add file or link', ''))
+        self.pad.append((35, 0, '- Add a nzb file location or URL to sabnzbd+', ''))
+        self.pad.append((8,  1, 'URL or file :', ''))
 
-        self.window.pad.addStr('   Add newzbin')
-        self.window.pad.addString(3, 35, '- Add a nzb from Newzbin.com to sabnzbd+ by entering a Newzbin Id')
-        self.window.pad.addString(4, 8, 'Newzbin ID :\n\n')
+        self.pad.append((3,  3, 'Add newzbin', ''))
+        self.pad.append((35, 3, '- Add a nzb from Newzbin.com to sabnzbd+ by entering a Newzbin Id', ''))
+        self.pad.append((8,  4, 'Newzbin ID :', ''))
 
-        self.window.pad.addStr('   Speed limit: ' + apiInfo["speedlimit"])
-        self.window.pad.addString(6, 35, '- Limit the download speed of sabnzbd+ (Useful to avoid network congestion)')
-        self.window.pad.addString(7, 8, 'New limit :\n\n')
+        self.pad.append((3,  6, 'Speed limit: ' + apiInfo["speedlimit"], ''))
+        self.pad.append((35, 6, '- Limit the download speed of sabnzbd+ (Useful to avoid network congestion)', ''))
+        self.pad.append((8,  7, 'New limit :', ''))
 
-        self.window.pad.addStr('   Set temporary pause')
-        self.window.pad.addString(9, 35, '- Pause queue for X minutes\n')
-        self.window.pad.addString(10, 8, 'Duration :\n\n')
+        self.pad.append((3,  9, 'Set temporary pause', ''))
+        self.pad.append((35, 9, '- Pause queue for X minutes\n', ''))
+        self.pad.append((8,  10, 'Duration :', ''))
 
-        self.window.pad.addStr('   Set queue completed script')
-        self.window.pad.addString(12, 35, '- Enter path to a local shell script that will be executed once sabnzbd+ finishes its queue\n\n')
-        self.window.pad.addString(13, 8, 'Path :\n\n')
+        self.pad.append((3,  12, 'Set queue completed script', ''))
+        self.pad.append((35, 12, '- Enter path to a local shell script that will be executed once sabnzbd+ finishes its queue\n\n', ''))
+        self.pad.append((8,  13, 'Path :', ''))
 
-        self.window.pad.addStr("   Generate newapikey")
-        self.window.pad.addString(15, 35, '- Change the current API Key of sabnzbd+ (WARNING this will disable use of SABCurses)\n')
-        self.window.pad.addString(16, 8, 'Are you Sure? :\n')
-        self.window.pad.addString(16, 35, "- Write 'yes' to confirm")
+        self.pad.append((3,  15, "Generate newapikey", ''))
+        self.pad.append((35, 15, '- Change the current API Key of sabnzbd+ (WARNING this will disable use of SABCurses)\n', ''))
+        self.pad.append((8,  16, 'Are you Sure? :', ''))
+        self.pad.append((35, 16, "- Write 'yes' to confirm", ''))
 
-        return True
-
-    '''
-    #        self.run_commands('version',  None)
-
+'''
 #        if APIVERSION != self.retval:
 #            self.stdscr.addstr(1, 3, "WARNING:", curses.color_pair(2))
 #            self.stdscr.addstr(" API version mismatch between SABCurses (%s) and daemon (%s)" % (APIVERSION, self.retval ) )
