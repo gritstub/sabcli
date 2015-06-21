b800662f29bfaebeaef4f3f873bf9cf350209dfa
HenrikDK <noreply@email.com>
2015-06-21 21:56:07 +0200

    added test suite of coverage of presentation layer

---
+++ unittest/presenter_Test/sabHelpPresenter_Test.py
@@ -0,0 +1,89 @@
+import nose
+import BaseTestCase
+from mock import Mock
+from cursesUI import cursesWindow
+from presenters.sabHelpPresenter import sabHelpPresenter
+
+
+class sabHelpPresenter_Test(BaseTestCase.BaseTestCase):
+
+    def setUp(self):
+        super(self.__class__, self).setUp()
+        self.test_presenter = sabHelpPresenter(window=Mock(spec=cursesWindow.cursesWindow))
+
+    def test_should_instantiate(self):
+        assert(isinstance(self.test_presenter, sabHelpPresenter))
+
+    def test_display_should_call_displayGeneralInformation(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayNavigationInformation = Mock()
+        self.test_presenter.displayQueueViewInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.displayGeneralInformation.assert_called_with()
+
+    def test_display_should_call_displayNavigationInformation(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayNavigationInformation = Mock()
+        self.test_presenter.displayQueueViewInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.displayNavigationInformation.assert_called_with()
+
+    def test_display_should_call_displayQueueViewInformation(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayNavigationInformation = Mock()
+        self.test_presenter.displayQueueViewInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.displayQueueViewInformation.assert_called_with()
+
+    def test_display_should_draw_window_instructions(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayNavigationInformation = Mock()
+        self.test_presenter.displayQueueViewInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.window.draw.assert_called_with([])
+
+    def test_display_should_draw_pad_instructions(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayNavigationInformation = Mock()
+        self.test_presenter.displayQueueViewInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.window.pad.draw.assert_called_with([])
+
+    def test_prepare_should_add_screen_instructions(self):
+        self.test_presenter.window.size = ["1", "1"]
+        self.test_presenter.displayNavigationInformation = Mock()
+        self.test_presenter.displayQueueViewInformation = Mock()
+
+        self.test_presenter.displayGeneralInformation()
+
+        assert len(self.test_presenter.screen) == 2
+
+    def test_displayGeneralInformation_should_update_pad_with_general_information(self):
+
+        self.test_presenter.displayNavigationInformation()
+
+        assert len(self.test_presenter.pad) == 18
+
+    def test_displayQueueViewInformation_should_update_pad_with_queue_information(self):
+
+        self.test_presenter.displayQueueViewInformation()
+
+        assert len(self.test_presenter.pad) == 9
+
+if __name__ == '__main__':
+    nose.runmodule()
---
+++ unittest/presenter_Test/sabMiscPresenter_Test.py
@@ -0,0 +1,69 @@
+import nose
+import BaseTestCase
+from mock import Mock
+from cursesUI import cursesWindow
+from presenters.sabMiscPresenter import sabMiscPresenter
+
+
+class sabMiscPresenter_Test(BaseTestCase.BaseTestCase):
+
+    def setUp(self):
+        super(self.__class__, self).setUp()
+        self.test_presenter = sabMiscPresenter(window=Mock(spec=cursesWindow.cursesWindow))
+
+    def test_should_instantiate(self):
+        assert(isinstance(self.test_presenter, sabMiscPresenter))
+
+    def test_display_should_call_displayGeneralInformation(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayMiscellaneousInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.displayGeneralInformation.assert_called_with({})
+
+    def test_display_should_call_displayMiscellaneousInformation(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayMiscellaneousInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.displayMiscellaneousInformation.assert_called_with({})
+
+    def test_display_should_draw_window_instructions(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayMiscellaneousInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.window.draw.assert_called_with([])
+
+    def test_display_should_draw_pad_instructions(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayMiscellaneousInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display()
+
+        self.test_presenter.window.pad.draw.assert_called_with([])
+
+    def test_displayGeneralInformation_should_add_screen_instructions(self):
+        self.test_presenter.window.size = ["1", "1"]
+        self.test_presenter.displayNavigationInformation = Mock()
+        self.test_presenter.displayQueueViewInformation = Mock()
+
+        self.test_presenter.displayGeneralInformation({"version": "1.0.0"})
+
+        assert len(self.test_presenter.screen) == 3
+
+    def test_displayMiscellaneousInformation_should_update_pad_with_general_information(self):
+
+        self.test_presenter.displayMiscellaneousInformation({"speedlimit": "300"})
+
+        assert len(self.test_presenter.pad) == 19
+
+if __name__ == '__main__':
+    nose.runmodule()
