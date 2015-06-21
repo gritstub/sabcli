2bb45bf88603b98448bbe03793ea7cbe062cb316
HenrikDK <noreply@email.com>
2015-06-21 23:36:35 +0200

    restructured to decrease interface between presenter and curses api

--- sabcli/presenters/sabHistoryPresenter.py
+++ sabcli/presenters/sabHistoryPresenter.py
@@ -1,57 +1,17 @@
-import sys
-import curses
 from cursesUI.cursesWindow import cursesWindow
 
 
 class sabHistoryPresenter:
-    def __init__(self, , window = cursesWindow(), scroller = sabScroller()):
+    def __init__(self, window = None):
+        if not window:
+            window = cursesWindow()
         self.window = window
-        self.scroller = scroller
 
+        self.screen = []
+        self.pad = []
 
-    def displayUnpackStatus(self, item):
-        if item.has_key("unpack_status"):
-            self.window.pad.addStr('[unpack: ')
-            if item["unpack_status"] > 0:
-                self.window.pad.addStr('FAIL', curses.color_pair(2))
-            else:
-                self.window.pad.addStr('OK', curses.color_pair(3))
-            self.window.pad.addStr(']')
-
-    def displayRepairStatus(self, item):
-        if item.has_key("repair_status"):
-            self.window.pad.addStr('[par2: ')
-            if item["repair_status"] > 0:
-                self.window.pad.addStr('FAIL', curses.color_pair(2))
-            else:
-                self.window.pad.addStr('OK', curses.color_pair(3))
-            self.window.pad.addStr(']')
-
-    def displayRepairLog(self, item):
-        if item.has_key("repair_log"):
-            for log_item in item["repair_log"]:
-                if log_item.find("Repair failed") > -1:
-                    part1, part2 = log_item.split("Repair failed")
-                    self.window.pad.addStr("       " + part1)
-                    self.window.pad.addStr("Repair failed" + part2 + '\n', curses.color_pair(2))
-                else:
-                    self.window.pad.addStr("       " + log_item + '\n')
-
-    def displayUnpackLog(self, item):
-        if item.has_key("unpack_log"):
-            for log_item in item["unpack_log"]:
-                self.window.pad.addStr("       " + log_item + '\n')
-
-    def displayDownloadInformation(self, index, item):
-        if index < 10:
-            self.window.pad.addStr(' ')
-        self.window.pad.addStr('  ' + str(index), curses.color_pair(3))
-        self.window.pad.addStr(' - ' + item["name"] + '\n')
-        self.window.pad.addStr('       [download: ' + item['size'] + '] ')
-
-    def display(self, state = {}):
-        self.window.addString(2, 3, '# - Filename\n')
-        self.window.addString(3, 0, '-' * int(self.window.size[1] + '\n'))
+    def display(self, state):
+        self.displayGeneralInformation()
 
         for index, item in enumerate(state["history"]):
 
@@ -68,3 +28,53 @@ class sabHistoryPresenter:
             self.window.pad.addStr('\n')
 
         self.scroller.display(state)
+
+    def displayGeneralInformation(self):
+        self.window.addString(2, 3, '# - Filename\n', '')
+        self.window.addString(3, 0, '-' * int(self.window.size[1] + '\n'), '')
+
+    def displayDownloadInformation(self, index, item):
+        self.pad.append(('  ' + str(index), 3))
+        self.pad.append((' - ' + item["name"] + '\n', ''))
+        self.pad.append(('       [size: ' + item['size'] + '] ', ''))
+
+    def displayRepairStatus(self, item):
+        if not item.has_key("repair_status"):
+            return
+
+        self.pad.append(('[par2: ', ''))
+        if item["repair_status"] > 0:
+            self.pad.append(('FAIL', 2))
+        else:
+            self.pad.append(('OK', 3))
+        self.pad.append((']', ''))
+
+    def displayUnpackStatus(self, item):
+        if not item.has_key("unpack_status"):
+            return
+
+        self.pad.append(('[unpack: ', ''))
+        if item["unpack_status"] > 0:
+            self.pad.append(('FAIL', 2))
+        else:
+            self.pad.append(('OK', 3))
+        self.pad.append((']', ''))
+
+    def displayRepairLog(self, item):
+        if not item.has_key("repair_log"):
+            return
+
+        for log_item in item["repair_log"]:
+            if log_item.find("Repair failed") > -1:
+                part1, part2 = log_item.split("Repair failed")
+                self.pad.append(("       " + part1, ''))
+                self.pad.append(("Repair failed" + part2 + '\n', 2))
+            else:
+                self.pad.append(("       " + log_item + '\n', ''))
+
+    def displayUnpackLog(self, item):
+        if not item.has_key("unpack_log"):
+            return
+
+        for log_item in item["unpack_log"]:
+            self.pad.append(("       " + log_item + '\n', ''))
