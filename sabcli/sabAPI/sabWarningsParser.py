2cc115d4c12e66b24e28cc368023a9449cb068fc
HenrikDK <noreply@email.com>
2015-06-21 21:39:33 +0200

    restructured class internals to increase readability

--- sabcli/sabAPI/sabWarningsParser.py
+++ sabcli/sabAPI/sabWarningsParser.py
@@ -1,20 +1,24 @@
 class sabWarningsParser():
 
-    def parse(self, response = {}):
+    def parse(self, response):
         state = {}
-        warnings = response["warnings"]
+        state["warnings"] = self.parseWarnings(response["warnings"])
 
+        return state
+
+    def parseWarnings(self, warnings):
         log = []
+
         for warning in warnings:
             warning = warning.split("\n")
-            warning[2] = warning[2].replace("WARNING: ","")
-            warning[2] = warning[2].replace("filename=","")
-            warning[2] = warning[2].replace(", type=None","")
+            warning[2] = warning[2].replace("WARNING: ", "")
+            warning[2] = warning[2].replace("filename=", "")
+            warning[2] = warning[2].replace(", type=None", "")
             entry = {
-                "timestamp" : warning[0],
+                "timestamp": warning[0],
                 "type": warning[1],
                 "message": warning[2]
             }
             log.append(entry)
-        state["warnings"] = log
-        return state
+
+        return log
