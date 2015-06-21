dbdd237970ba1f1660adc4ed6720f1409a95d7f2
HenrikDK <noreply@email.com>
2015-06-21 21:38:39 +0200

    restructured class internals to increase readability

--- sabcli/sabAPI/sabHistoryParser.py
+++ sabcli/sabAPI/sabHistoryParser.py
@@ -1,5 +1,50 @@
 import time
+
+
 class sabHistoryParser():
+
+    def parse(self, response):
+        state = {"last_fetch_time": time.time(),
+                 "status": response["history"]["status"],
+                 "speed": "%.2f kb/s" % float(response["history"]["kbpersec"]),
+                 "size_left": response["history"]["sizeleft"],
+                 "time_left": response["history"]["timeleft"],
+                 "mb": response["history"]["mb"],
+                 "mbleft": response["history"]["mbleft"],
+                 "diskspace2": response["history"]["diskspace2"],
+                 "diskspacetotal2": response["history"]["diskspacetotal2"],
+                 "total_size": response["history"]["total_size"],
+                 "month_size": response["history"]["month_size"],
+                 "week_size": response["history"]["week_size"]
+        }
+
+        state["history"] = self.parseHistory(response)
+        return state
+
+    def parseHistory(self, response):
+        slots = response["history"]['slots']
+
+        history = []
+        for slot in slots:
+            item = {"index": slot["id"] - 1,
+                    "name": slot["name"],
+                    "size": slot["size"],
+                    "id": slot["nzo_id"],
+                    "status": slot["status"],
+            }
+
+            stage_log = self.extractStageLog(slot)
+
+            for action in stage_log:
+                if action["name"] == "Unpack":
+                    self.extractUnpackOperationStatus(action, item)
+
+                if action["name"] == "Repair":
+                    self.extractRepairOperationStatus(action, item)
+
+            history.append(item)
+        history.reverse()
+        return history
 
     def extractStageLog(self, slot):
         actions = slot["stage_log"]
@@ -16,56 +61,13 @@ class sabHistoryParser():
         item["unpack_fail"] = unpack_fail
         item["unpack_log"] = unpack_log
 
-    def parseHistory(self, response):
-        slots = response["history"]['slots']
-
-        history = []
-        for slot in slots:
-            item = {"index": slot["id"] - 1,
-                    "name": slot["name"],
-                    "size": slot["size"],
-                    "id":slot["nzo_id"],
-                    "status": slot["status"],
-            }
-
-            stage_log = self.extractStageLog(slot)
-
-            for action in stage_log:
-                if action['name'] == "Unpack":
-                    self.extractUnpackOperationStatus(action, item)
-
-                if action['name'] == "Repair":
-                    self.extractRepairOperationStatus(action, item)
-
-            history.append(item)
-        history.reverse()
-        return history
-
-    def parse(self, response = {}):
-        state = {'last_fetch_time':time.time(),
-                 "status":response["history"]["status"],
-                 "speed":"%.2f kb/s" % float(response["history"]["kbpersec"]),
-                 "size_left":response["history"]["sizeleft"],
-                 "time_left":response["history"]["timeleft"],
-                 "mb":response["history"]["mb"],
-                 "mbleft":response["history"]["mbleft"],
-                 "diskspace2":response["history"]["diskspace2"],
-                 "diskspacetotal2":response["history"]["diskspacetotal2"],
-                 "total_size":response["history"]["total_size"],
-                 "month_size":response["history"]["month_size"],
-                 "week_size":response["history"]["week_size"]
-        }
-
-        state["history"] = self.parseHistory(response)
-        return state
-
     def extractRepairOperationStatus(self, action, item):
         repair_log = []
-        repair_fail= 0
+        repair_fail = 0
         for error_string in action['actions']:
-            if error_string.find('Quick Check OK') < 0 and error_string.find('Repaired in') < 0 and error_string.find('all files correct') < 0:
+            if error_string.find('Quick Check OK') < 0 and error_string.find('Repaired in') < 0 and error_string.find("all files correct") < 0:
                 repair_log.append(error_string)
-                repair_fail= 1
+                repair_fail = 1
 
         item["repair_status"] = repair_fail
         item["repair_log"] = repair_log
