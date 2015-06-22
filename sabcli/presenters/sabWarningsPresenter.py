a0032f197b2812bb6820147ebb292578e42c1610
HenrikDK <noreply@email.com>
2015-06-23 00:05:28 +0200

    restructure to increase readability

--- sabcli/presenters/sabWarningsPresenter.py
+++ sabcli/presenters/sabWarningsPresenter.py
@@ -15,9 +15,7 @@ class sabWarningsPresenter():
         self.pad = []
 
         self.displayGeneralInformation()
-
-        for warning in state["warnings"]:
-            self.displayWarningInformation(warning)
+        self.displayWarnings(state)
 
         self.window.draw(self.screen)
         self.window.pad.draw(self.pad)
@@ -26,12 +24,16 @@ class sabWarningsPresenter():
         self.screen.append((3, 2, '[Date/Timestamp]          Type     | System message\n', ''))
         self.screen.append((0, 3, '-' * int(self.window.size[1] + '\n'), ''))
 
+    def displayWarnings(self, state):
+        for warning in state["warnings"]:
+            self.displayWarningInformation(warning)
+
     def displayWarningInformation(self, warning):
         self.pad.append(('   [' + warning["timestamp"] + '] ', ''))
         if warning["type"] == 'ERROR':
             self.pad.append((warning["type"], 2))
         else:
-            self.pad.append((warning["type"]), '')
+            self.pad.append((warning["type"], ''))
 
         warnings_length = len(warning["timestamp"]) + len(warning["type"])
         if warnings_length < 32:
