799be992219be3831ec588be368d1ed0ed1719ca
HenrikDK <noreply@email.com>
2015-06-25 22:15:05 +0200

    updated queue presenter to use a fixed lenght scroll presenter

--- unittest/presenter_Test/sabQueuePresenter_Test.py
+++ unittest/presenter_Test/sabQueuePresenter_Test.py
@@ -1,15 +1,17 @@
 import nose
 import BaseTestCase
 from mock import Mock
-from cursesUI import cursesWindow
+from cursesUI.cursesWindow import cursesWindow
 from presenters.sabQueuePresenter import sabQueuePresenter
+from presenters.sabScrollPresenter import sabScrollPresenter
 
 
 class sabQueuePresenter_Test(BaseTestCase.BaseTestCase):
 
     def setUp(self):
         super(self.__class__, self).setUp()
-        self.test_presenter = sabQueuePresenter(window=Mock(spec=cursesWindow.cursesWindow))
+        self.test_presenter = sabQueuePresenter(window=Mock(spec=cursesWindow),
+                                                scrollPresenter=Mock(spec=sabScrollPresenter))
 
     def test_should_instantiate(self):
         assert(isinstance(self.test_presenter, sabQueuePresenter))
@@ -30,7 +32,7 @@ class sabQueuePresenter_Test(BaseTestCase.BaseTestCase):
 
         self.test_presenter.display({})
 
-        self.test_presenter.displayQueue.assert_called_with({})
+        self.test_presenter.displayQueue.assert_called_with({"item_size": 3})
 
     def test_display_should_draw_window_instructions(self):
         self.test_presenter.displayGeneralInformation = Mock()
@@ -41,6 +43,15 @@ class sabQueuePresenter_Test(BaseTestCase.BaseTestCase):
 
         self.test_presenter.window.draw.assert_called_with([])
 
+    def test_display_should_call_scrollpresenter_display(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayQueue = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display({})
+
+        self.test_presenter.scrollPresenter.display.assert_called_with({"item_size": 3})
+
     def test_display_should_draw_pad_instructions(self):
         self.test_presenter.displayGeneralInformation = Mock()
         self.test_presenter.displayQueue = Mock()
@@ -49,6 +60,16 @@ class sabQueuePresenter_Test(BaseTestCase.BaseTestCase):
         self.test_presenter.display({})
 
         self.test_presenter.window.pad.draw.assert_called_with([])
+
+    def test_display_should_draw_pad_instructions(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayQueue = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display({})
+
+        self.test_presenter.window.pad.draw.assert_called_with([])
+
 
     def test_displayGeneralInformation_should_add_screen_instructions_if_views_are_present(self):
         self.test_presenter.window.size = ["1", "1"]
