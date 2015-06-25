23156a1a4bba8e8dbdd14231ef43ac5470aa4ce3
HenrikDK <noreply@email.com>
2015-06-26 00:39:08 +0200

    ensure that misc controller can highlight items using scroll presenter

--- sabcli/controllers/sabMiscController.py
+++ sabcli/controllers/sabMiscController.py
@@ -12,14 +12,19 @@ class sabMiscController():
             miscPresenter = sabMiscPresenter()
         self.miscPresenter = miscPresenter
 
+        self.item_length = 6
         self.selected = False
         self.cached = False
+        self.selectedItem = -1
 
     def update(self):
         self.state = self.api.getApiInfo()
+        self.state["list_length"] = 6
         return self.state
 
     def display(self):
+        self.state["current_index"] = self.selectedItem
+        self.state["list_length"] = self.selectedItem
         self.miscPresenter.display(self.state)
 
     def handleInput(self, keyPressed):
@@ -28,16 +33,39 @@ class sabMiscController():
         if not self.selected:
             return handled
 
-        elif keyPressed == 259: # Curser up
-            handled = self.handleUp(handled)
+        elif keyPressed == 259:  # Cursor up
+            handled = self.handleUp()
 
-        elif keyPressed == 258:  # Curser down
-            handled = self.handleDown(handled)
+        elif keyPressed == 258:  # Cursor down
+            handled = self.handleDown()
 
+        elif keyPressed == 260:  # Left
+            handled = self.handleLeft()
+
+        elif keyPressed == 261:  # Right
+            handled = self.handleRight()
+
+        self.selected = self.selectedItem != -1
         return handled
 
-    def handleDown(self, handled):
-        return handled
+    def handleUp(self):
+        if self.selectedItem > -1:
+            self.selectedItem -= 1
+        else:
+            self.selectedItem = self.item_length - 1
+        return True
 
-    def handleUp(self, handled):
-        return handled
+    def handleDown(self):
+        if self.selectedItem < self.item_length - 1:
+            self.selectedItem += 1
+        else:
+            self.selectedItem = -1
+        return True
+
+    def handleLeft(self):
+        self.selected = False
+        return False
+
+    def handleRight(self):
+        self.selected = False
+        return False
