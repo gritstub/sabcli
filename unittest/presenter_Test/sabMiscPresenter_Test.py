1f62b04d5a0df016d25994f923be0d4a93a60cd7
HenrikDK <noreply@email.com>
2015-06-26 00:31:07 +0200

    added scroller to misc view

--- unittest/presenter_Test/sabMiscPresenter_Test.py
+++ unittest/presenter_Test/sabMiscPresenter_Test.py
@@ -1,18 +1,19 @@
 import nose
 import BaseTestCase
 from mock import Mock
-from cursesUI import cursesWindow
+from cursesUI.cursesWindow import cursesWindow
 from presenters.sabMiscPresenter import sabMiscPresenter
+from presenters.sabScrollPresenter import sabScrollPresenter
 
 
 class sabMiscPresenter_Test(BaseTestCase.BaseTestCase):
-
     def setUp(self):
         super(self.__class__, self).setUp()
-        self.test_presenter = sabMiscPresenter(window=Mock(spec=cursesWindow.cursesWindow))
+        self.test_presenter = sabMiscPresenter(window=Mock(spec=cursesWindow),
+                                               scrollPresenter=Mock(spec=sabScrollPresenter))
 
     def test_should_instantiate(self):
-        assert(isinstance(self.test_presenter, sabMiscPresenter))
+        assert (isinstance(self.test_presenter, sabMiscPresenter))
 
     def test_display_should_call_displayGeneralInformation(self):
         self.test_presenter.displayGeneralInformation = Mock()
@@ -50,6 +51,15 @@ class sabMiscPresenter_Test(BaseTestCase.BaseTestCase):
 
         self.test_presenter.window.pad.draw.assert_called_with([])
 
+    def test_display_should_call_scrollPresenter(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayMiscellaneousInformation = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display({})
+
+        self.test_presenter.scrollPresenter.display({})
+
     def test_displayGeneralInformation_should_add_screen_instructions(self):
         self.test_presenter.window.size = ["1", "1"]
         self.test_presenter.displayNavigationInformation = Mock()
@@ -60,10 +70,10 @@ class sabMiscPresenter_Test(BaseTestCase.BaseTestCase):
         assert len(self.test_presenter.screen) == 3
 
     def test_displayMiscellaneousInformation_should_update_pad_with_general_information(self):
-
         self.test_presenter.displayMiscellaneousInformation({"speedlimit": "300"})
 
         assert len(self.test_presenter.pad) == 19
+
 
 if __name__ == '__main__':
     nose.runmodule()
