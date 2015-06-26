53984718798d333bbcc436b5549b5a4b4c1e251b
HenrikDK <noreply@email.com>
2015-06-26 23:41:43 +0200

    add support for dynamic formatting of message to warnings presenter add support for scrolling of multiple warnings to warnings presenter

--- unittest/presenter_Test/sabWarningsPresenter_Test.py
+++ unittest/presenter_Test/sabWarningsPresenter_Test.py
@@ -16,6 +16,7 @@ class sabWarningsPresenter_Test(BaseTestCase.BaseTestCase):
 
     def test_display_should_call_displayGeneralInformation(self):
         self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayScrollAndItemSelection = Mock()
         self.test_presenter.displayWarnings = Mock()
         self.test_presenter.window.pad = Mock()
 
@@ -25,6 +26,7 @@ class sabWarningsPresenter_Test(BaseTestCase.BaseTestCase):
 
     def test_display_should_call_displayWarnings(self):
         self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayScrollAndItemSelection = Mock()
         self.test_presenter.displayWarnings = Mock()
         self.test_presenter.window.pad = Mock()
 
@@ -34,6 +36,7 @@ class sabWarningsPresenter_Test(BaseTestCase.BaseTestCase):
 
     def test_display_should_draw_window_instructions(self):
         self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayScrollAndItemSelection = Mock()
         self.test_presenter.displayWarnings = Mock()
         self.test_presenter.window.pad = Mock()
 
@@ -43,6 +46,7 @@ class sabWarningsPresenter_Test(BaseTestCase.BaseTestCase):
 
     def test_display_should_draw_pad_instructions(self):
         self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayScrollAndItemSelection = Mock()
         self.test_presenter.displayWarnings = Mock()
         self.test_presenter.window.pad = Mock()
 
@@ -50,18 +54,79 @@ class sabWarningsPresenter_Test(BaseTestCase.BaseTestCase):
 
         self.test_presenter.window.pad.draw.assert_called_with([])
 
+    def test_display_should_call_displayScrollAndItemSelection(self):
+        self.test_presenter.displayGeneralInformation = Mock()
+        self.test_presenter.displayScrollAndItemSelection = Mock()
+        self.test_presenter.displayWarnings = Mock()
+        self.test_presenter.window.pad = Mock()
+
+        self.test_presenter.display({})
+
+        self.test_presenter.window.pad.draw.assert_called_with([])
+
+    def test_displayGeneralInformation_should_update_screen_with_instructions(self):
+        self.test_presenter.window.size = ["50", "50"]
+
+        self.test_presenter.displayGeneralInformation()
+
+        assert len(self.test_presenter.screen) == 2
+
     def test_displayWarning_should_call_displayWarningInformation(self):
         self.test_presenter.displayWarningInformation = Mock()
 
         self.test_presenter.displayWarnings({"warnings": [{}]})
 
-        self.test_presenter.displayWarningInformation.assert_called_with({})
+        self.test_presenter.displayWarningInformation.assert_called_with({"item_length": 1})
 
     def test_displayWarningInformation_should_add_pad_instructions(self):
+        self.test_presenter.window.size = ["50", "50"]
+        self.test_presenter.fitWarningMessageToWindow = Mock()
+        self.test_presenter.displayWarningMessage = Mock()
 
-        self.test_presenter.displayWarningInformation({"timestamp": "00:00:00", "type": "test_type", "message":"test_message"})
+        self.test_presenter.displayWarningInformation({"timestamp": "00:00:00", "type": "test_type", "message": "test_message"})
 
-        assert len(self.test_presenter.pad) == 5
+        assert len(self.test_presenter.pad) == 4
+
+    def test_displayWarningInformation_should_call_fitWarningMessageToWindow(self):
+        self.test_presenter.window.size = ["50", "50"]
+        self.test_presenter.fitWarningMessageToWindow = Mock()
+        self.test_presenter.displayWarningMessage = Mock()
+
+        self.test_presenter.displayWarningInformation({"timestamp": "00:00:00", "type": "test_type", "message": "test_message"})
+
+        self.test_presenter.fitWarningMessageToWindow.assert_called_with({"timestamp": "00:00:00", "type": "test_type", "message": "test_message"})
+
+    def test_displayWarningInformation_should_call_displayWarningMessage(self):
+        self.test_presenter.window.size = ["50", "50"]
+        self.test_presenter.fitWarningMessageToWindow = Mock()
+        self.test_presenter.displayWarningMessage = Mock()
+
+        self.test_presenter.displayWarningInformation({"timestamp": "00:00:00", "type": "test_type", "message": "test_message"})
+
+        self.test_presenter.displayWarningMessage.assert_called_with({"timestamp": "00:00:00", "type": "test_type", "message": "test_message"})
+
+    def test_fitWarningMessageToWindow_should_populate_item_length(self):
+        self.test_presenter.window.size = ["50", "45"]
+
+        warning = {"timestamp": "00:00:00", "type": "test_type", "message": "test1 test2 test3"}
+        self.test_presenter.fitWarningMessageToWindow(warning)
+
+        assert "item_length" in warning
+
+    def test_fitWarningMessageToWindow_should_populate_lines(self):
+        self.test_presenter.window.size = ["50", "45"]
+
+        warning = {"timestamp": "00:00:00", "type": "test_type", "message": "test1 test2 test3"}
+        self.test_presenter.fitWarningMessageToWindow(warning)
+
+        assert "lines" in warning
+
+    def test_displayWarningMessage_should_add_pad_instructions(self):
+        self.test_presenter.window.size = ["50", "45"]
+
+        self.test_presenter.displayWarningMessage({"lines": ["line 1", "line 2", "line 3"]})
+
+        assert len(self.test_presenter.pad) == 6
 
 if __name__ == '__main__':
     nose.runmodule()
