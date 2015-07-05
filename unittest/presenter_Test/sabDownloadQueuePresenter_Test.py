04fb07bb11b0db3ad54d90a1a587198c58fc416a
HenrikDK <noreply@email.com>
2015-07-05 23:35:57 +0200

    name change

--- unittest/presenter_Test/sabDownloadQueuePresenter_Test.py
+++ unittest/presenter_Test/sabDownloadQueuePresenter_Test.py
@@ -2,7 +2,7 @@ import nose
 import BaseTestCase
 from mock import Mock
 from cursesUI.cursesWindow import cursesWindow
-from presenters.sabDownloadQueuePresenter import sabQueuePresenter
+from presenters.sabDownloadQueuePresenter import sabDownloadQueuePresenter
 from presenters.sabScrollPresenter import sabScrollPresenter
 
 
@@ -10,11 +10,10 @@ class sabQueuePresenter_Test(BaseTestCase.BaseTestCase):
 
     def setUp(self):
         super(self.__class__, self).setUp()
-        self.test_presenter = sabQueuePresenter(window=Mock(spec=cursesWindow),
-                                                scrollPresenter=Mock(spec=sabScrollPresenter))
+        self.test_presenter = sabDownloadQueuePresenter(window=Mock(spec=cursesWindow), scrollPresenter=Mock(spec=sabScrollPresenter))
 
     def test_should_instantiate(self):
-        assert(isinstance(self.test_presenter, sabQueuePresenter))
+        assert(isinstance(self.test_presenter, sabDownloadQueuePresenter))
 
     def test_display_should_call_displayGeneralInformation(self):
         self.test_presenter.displayGeneralInformation = Mock()
@@ -126,8 +125,7 @@ class sabQueuePresenter_Test(BaseTestCase.BaseTestCase):
 
         self.test_presenter.displayFileInformation({"index": 0, "filename": "test_name", "avg_age": "3 Days",
                                                     "priority": "high", "unpackopts": "1", "status": "paused"})
-
-        assert len(self.test_presenter.pad) == 12
+        assert len(self.test_presenter.pad) == 13
         assert len(self.test_presenter.screen) == 0
 
     def test_displayFileInformation_should_highlight_filename(self):
@@ -135,21 +133,21 @@ class sabQueuePresenter_Test(BaseTestCase.BaseTestCase):
         self.test_presenter.displayFileInformation({"index": 0, "filename": "test_name", "avg_age": "3 Days",
                                                     "priority": "high", "unpackopts": "1", "status": "paused"}, 0)
 
-        assert self.test_presenter.pad[2][1] == 2
+        assert self.test_presenter.pad[3][1] == 2
 
     def test_displayFileInformation_should_highlight_priority(self):
 
         self.test_presenter.displayFileInformation({"index": 0, "filename": "test_name", "avg_age": "3 Days",
                                                     "priority": "high", "unpackopts": "1", "status": "paused"}, 1)
 
-        assert self.test_presenter.pad[6][1] == 2
+        assert self.test_presenter.pad[7][1] == 2
 
     def test_displayFileInformation_should_highlight_unpack_settings(self):
 
         self.test_presenter.displayFileInformation({"index": 0, "filename": "test_name", "avg_age": "3 Days",
                                                     "priority": "high", "unpackopts": "1", "status": "paused"}, 2)
 
-        assert self.test_presenter.pad[8][1] == 2
+        assert self.test_presenter.pad[9][1] == 2
 
     def test_displayProgress_should_add_pad_instructions(self):
         self.test_presenter.window.size = ["1", "1"]
@@ -159,12 +157,26 @@ class sabQueuePresenter_Test(BaseTestCase.BaseTestCase):
         assert len(self.test_presenter.pad) == 11
         assert len(self.test_presenter.screen) == 0
 
-    def test_wait_should_call_window_wait(self):
-        self.test_presenter.window.size = ["1", "1"]
+    def test_editDownloadName_should_call_scrollPresenter_to_get_view_ports_current_line(self):
+        self.test_presenter.scrollPresenter.calculateScrollingLine = Mock(return_value = 1)
+        self.test_presenter.window = Mock()
+        state = {"item_size": 2, "queue": [{}, {"filename": "test"}]}
 
-        self.test_presenter.wait(100)
+        self.test_presenter.editDownloadName(1, state)
 
-        self.test_presenter.window.wait.assert_called_with(100)
+        self.test_presenter.scrollPresenter.calculateScrollingLine.assert_called_with(state)
+
+    def test_editDownloadName_should_calculate_current_editing_line_based_on_view_ports_current_line(self):
+        self.test_presenter.scrollPresenter.calculateScrollingLine = Mock(return_value = 0)
+        self.test_presenter.window = Mock()
+        state = {"item_size": 3, "queue": [{}, {"filename": "test"}]}
+
+        self.test_presenter.editDownloadName(1, state)
+
+        line_pad_index = 3 * 1
+        line_scroll_index = 0
+        edit_line = line_pad_index - line_scroll_index + 4
+        self.test_presenter.window.editTextOnScreen.assert_called_with(edit_line, state["queue"][1]["filename"])
 
 if __name__ == '__main__':
     nose.runmodule()
